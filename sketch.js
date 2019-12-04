12dfe4e40430b4d06f0af4e4e38c50efc3b4ee50
noor bondogji <noor.bondogji@sjsu.edu>
2019-12-03 17:34:37 -0800

    psuedocode

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
-let player;
+// assign variables for each functions that'll be used.
+
+let player; // main player
 
 let playerX = 300;
 let playerY = 385;
@@ -7,17 +9,18 @@ let morphY = 200;
 let iX = 0;
 let iY = 0;
 let score = 1;
-let playerColor;
+let playerColor; // color changes
 
-let playerShape;
-let shapes= ["square", "ellipse"]
+let playerShape; // this will have the shapes change shapes everytime it hits the ball
+let shapes = ["square", "ellipse"]
 
-function setup() {
+function setup() { // this a setup for the window frame.
   frameRate = 2
   createCanvas(windowWidth, windowHeight);
 
+  // this changes the shapes colors everytime its reloaded
   playerColor = color(random(255), random(255), random(255))
- playerShape= random(shapes);
+  playerShape = random(shapes);
 
 
 }
@@ -33,54 +36,54 @@ function draw() {
 
 
 }
+// death is for when player hits the edges and the screen goes red. inttially should also say "you lost"
+function death() {
+  if (playerX > 590) {
+    background(200, 40, 50)
+    print("You lost!")
+  }
+  if (playerX < 10) {
+    background(200, 40, 50)
+    print("You lost!")
 
-  function death(){
-    if(playerX > 590){
-      background (200,40,50)
-      print("You lost!")
-    }
-    if(playerX < 10){
-   background (200,40,50)
-            print("You lost!")
+  }
+  if (playerY < 10) {
+    background(200, 40, 50)
+    print("You lost!")
 
-    }
-    if(playerY < 10){
-   background (200,40,50)
-            print("You lost!")
-
-    }
-    if(playerY > 390){
-      background (200,40,50)
-            print("You lost!")
-
-       }
-
-
-
+  }
+  if (playerY > 390) {
+    background(200, 40, 50)
+    print("You lost!")
 
   }
 
 
 
+
+}
+
+
+// player show shows the player that catches the ball.
 function playerShow() {
-  fill(playerColor);
+  fill(playerColor); // goes back to color change in player
   noStroke();
   ellipseMode(CENTER)
 
-if (playerShape=="ellipse"){
+  // for loop is for shapes to change when loses. We are trying to make it change without replaying game.
+  if (playerShape == "ellipse") {
 
-  ellipse(playerX, playerY, 25);
+    ellipse(playerX, playerY, 25);
+
+  } else if (playerShape == "square") {
+
+    rect(playerX, playerY, 25, 25)
+
+  }
 
 }
-else if (playerShape=="square") {
 
-  rect(playerX, playerY, 25, 25)
-
-}
-
-}
-
-
+// keyPressed is for the arrow keys that allows player move across screen easily.
 function keyPressed() {
   playerX = playerX + iX;
   playerY = playerY + iY;
@@ -104,10 +107,10 @@ function keyPressed() {
 
 
 function foodShow() {
-    let a = random(0,255);
-  let b = random(0,255);
-  let c = random(0,255);
-  fill(a,b,c)
+  let a = random(0, 255);
+  let b = random(0, 255);
+  let c = random(0, 255);
+  fill(a, b, c)
   noStroke()
   ellipseMode(CENTER)
   ellipse(morphX, morphY, 10)
@@ -121,15 +124,15 @@ function foodUpdate() {
   if (d < 15) {
     foodX = random(10, 590);
     foodY = random(10, 390);
-    print("+"+score)
+    print("+" + score)
   }
 
-  }
+}
 
-function scorePrint (){
-    let d = dist(playerX, playerY, morphX, morphY)
+function scorePrint() {
+  let d = dist(playerX, playerY, morphX, morphY)
 
-    if (d < 15) {
+  if (d < 15) {
     score++
   }
 

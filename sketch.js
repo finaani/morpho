d036facb227c085f32530f8e83931d95b89f7f9e
noor bondogji <noor.bondogji@sjsu.edu>
2019-12-03 16:10:04 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,136 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.dom.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.sound.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
 
+  </head>
+  <body>
+    <script src="sketch.js"></script>
+  </body>
+</html>let player;
 
-let spritesheet;
-let spritedata;
+let playerX = 300;
+let playerY = 385;
+let morphX = 300;
+let morphY = 200;
+let iX = 0;
+let iY = 0;
+let score = 1;
 
-let animation = [];
-
-let horses = [];
-
-function preload() {
-  spritedata = loadJSON('horse/horse.json');
-  spritesheet = loadImage('horse/horse.png');
-}
 
 
 function setup() {
-  createCanvas(640, 480);
-  let frames = spritedata.frames;
-  for (let i = 0; i < frames.length; i++) {
-    let pos = frames[i].position;
-    let img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
-    animation.push(img);
+  frameRate = 1
+  createCanvas(800, 400);
 
-
-
-  }
-
-  for (let i = 0; i < 5; i++) {
-    horses[i] = new Sprite(animation, 0, i * 75, random(0.1, 0.4));
-  }
 }
 
 function draw() {
-  background(0);
-  var circleX = random(width);
-var circleY = random(height);
-var circleSize = random(10, 100);
-fill(random(255), random(255), random(255));
-ellipse(circleX, circleY, circleSize)
+  background(0,203,108);
+  foodShow()
+  playerShow()
+  keyPressed()
+  foodUpdate()
+  death()
+  scorePrint()
 
 
-  for (let horse of horses) {
-    horse.show();
-    horse.animate();
+}
+
+  function death(){
+    if(playerX > 590){
+      background (200,40,50)
+      print("You lost!")
+    }
+    if(playerX < 10){
+   background (200,40,50)
+            print("You lost!")
+
+    }
+    if(playerY < 10){
+   background (200,40,50)
+            print("You lost!")
+
+    }
+    if(playerY > 390){
+      background (200,40,50)
+            print("You lost!")
+
+       }
+
+
+
+
   }
+
+
+
+function playerShow() {
+  fill(200, 20, 40);
+  noStroke();
+  ellipseMode(CENTER)
+  ellipse(playerX, playerY, 25)
+}
+
+
+function keyPressed() {
+  playerX = playerX + iX;
+  playerY = playerY + iY;
+  if (keyCode === UP_ARROW) {
+    iX = 0;
+    iY = -4;
+  }
+  if (keyCode === DOWN_ARROW) {
+    iX = 0;
+    iY = 4;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    iY = 0;
+    iX = 4;
+  }
+  if (keyCode === LEFT_ARROW) {
+    iY = 0;
+    iX = -4;
+  }
+}
+
+
+function foodShow() {
+    let a = random(0,255);
+  let b = random(0,255);
+  let c = random(0,255);
+  fill(a,b,c)
+  noStroke()
+  ellipseMode(CENTER)
+  ellipse(morphX, morphY, 10)
+
+
+}
+
+
+function foodUpdate() {
+  let d = dist(playerX, playerY, morphX, morphY)
+  if (d < 15) {
+    foodX = random(10, 590);
+    foodY = random(10, 390);
+    print("+"+score)
+  }
+
+  }
+
+function scorePrint (){
+    let d = dist(playerX, playerY, morphX, morphY)
+
+    if (d < 15) {
+    score++
+  }
+
+
+
+
+
 }
